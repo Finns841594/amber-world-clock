205c0254e59cd64f35616ef1ce34451b816168d4
Feng Y <fengy142857@gmail.com>
2023-02-27 17:21:41 +0100

    feat: added different clocks

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import moment from 'moment-timezone'
 import logo from './logo.svg';
 import './App.css';
 
+const cityList = ['Europe/Stockholm','America/Argentina/Rio_Gallegos','Asia/Macao']
+
 function App() {
+  let date = new Date()
+  const [time, setTime] = useState(date);
+ 
+  const Clock = ({city}: {city: string}) => {
+    return <div className="clock">Current time: { moment(time.getTime()).tz(city).format()} in {city}</div>
+  };
+  
+  const Board = () => {
+    const clocks = cityList.map(city => <Clock city={city} />)
+    console.log(clocks[0].props , clocks)
+    return <>{clocks}</>
+  }
+  
+  useEffect(() => {
+    setTimeout(() => setTime(new Date()), 1000);
+  }, [time]);
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section className='clock-area'>
+        <Board />
+        </section>
       </header>
     </div>
   );
